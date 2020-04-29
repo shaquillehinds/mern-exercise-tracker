5eba77b7d3b832eb71f8e1e71067bb2982517bcb
Shaq <shaqdulove@hotmail.com>
2020-04-29 11:53:11 -0400

    package

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri =
+  "mongodb+srv://shaquille:arsoniss1994@cluster0-khlai.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
