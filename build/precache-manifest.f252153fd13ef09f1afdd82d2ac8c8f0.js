16fbff23bb59e534ec12cdcab7d840a8a31a415d
Shaq <shaqdulove@hotmail.com>
2020-04-29 18:32:24 -0400

    added build folder

---
+++ build/precache-manifest.f252153fd13ef09f1afdd82d2ac8c8f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0ef18fe4a7ec433743e16b38d18f1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7c1bae2fb1fe01eab86",
+    "url": "/static/css/2.9e7b09f4.chunk.css"
+  },
+  {
+    "revision": "c7c1bae2fb1fe01eab86",
+    "url": "/static/js/2.6521d751.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.6521d751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d1e6129ab1a4f5a43e",
+    "url": "/static/js/main.ae575412.chunk.js"
+  },
+  {
+    "revision": "8de9e34e458c621c082f",
+    "url": "/static/js/runtime-main.31083b8b.js"
+  }
+]);
